d57ee8f9b66713491dbad1ea414509ef4e25b096
DanielAbimbolaa <153306503+DanielAbimbolaa@users.noreply.github.com>
2024-05-20 10:02:12 +0100

    Added properties to the userforregistrationdto and User Model

--- Models/DTOs/UserForRegistrationDto.cs
+++ Models/DTOs/UserForRegistrationDto.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using wepay.Models;
 
-public class UserForRegistrationDto
+public class UserForRegistrationDto :BaseEntity
 {
-	public UserForRegistrationDto()
-	{
-	}
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Address { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public string Password { get; set; }
 }

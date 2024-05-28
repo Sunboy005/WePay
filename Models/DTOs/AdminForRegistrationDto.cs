63dd82ebf24dea7d380cf03057c32bbc8f81a190
DanielAbimbolaa <153306503+DanielAbimbolaa@users.noreply.github.com>
2024-05-28 12:28:26 +0100

    Changed The Admin Registration to have required fields

--- Models/DTOs/AdminForRegistrationDto.cs
+++ Models/DTOs/AdminForRegistrationDto.cs
@@ -1,19 +1,23 @@
-﻿namespace wepay.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace wepay.Models.DTOs
 {
     public class AdminForRegistrationDto
     {
+        [Required(ErrorMessage = "First Name is required")]
         public string FirstName { get; set; }
-
+        [Required(ErrorMessage = "Last Name is required")]
         public string LastName { get; set; }
-
+        [Required(ErrorMessage = "User Name is required")]
         public string UserName { get; set; }
-
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
-
+        [Required(ErrorMessage = "Phone Number is required")]
+        [StringLength(11, ErrorMessage = "Phone Number must be 11 digits")]
         public string PhoneNumber { get; set; }
-
+        [Required(ErrorMessage = "Address is required")]
         public string Address { get; set; }
-
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
     }
 }
